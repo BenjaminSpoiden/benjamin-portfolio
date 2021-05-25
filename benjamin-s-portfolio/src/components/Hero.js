b3f450859a4c9f7a02e6d7f5c616c336fb29e2ec
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2021-05-25 09:38:30 +0200

    added a couple more projects

--- benjamin-s-portfolio/src/components/Hero.js
+++ benjamin-s-portfolio/src/components/Hero.js
@@ -16,7 +16,7 @@ export const Hero = () => {
             </div>
             <div className="hero-p">
                 <p>
-                    I'm a Belgium based front-end software engineer who specializes in building and designing website and mobile apps.
+                    I'm a Belgian based front-end software engineer who specializes in building and designing website and mobile apps.
                 </p>
             </div>
             <div style={{ marginTop:"70px" }}>
